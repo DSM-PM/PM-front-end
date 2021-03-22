cba066ee576d75143a4b2921f042a6ac49bea154
Srim <deamoon0801@naver.com>
2021-03-22 17:19:55 +0900

    [ADD] <SideView> UI

---
+++ pm-front-end/src/components/SideView/SideView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="side-container">
+    <img :src="sideViewImg" />
+  </div>
+</template>
+
+<script>
+import { sideViewImg } from "@/assets/img";
+export default {
+  data() {
+    return { sideViewImg };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$color: #f5df4d;
+.side-container {
+  width: 50%;
+  height: 100vh;
+  background-color: #fcf5c9;
+  display: flex;
+  justify-content: center;
+  img {
+    width: 45rem;
+  }
+}
+</style>
---
+++ pm-front-end/src/components/SideView/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <side-view />
+  </div>
+</template>
+
+<script>
+import { SideView } from "./";
+export default {
+  name: "SideView",
+  components: { "side-view": SideView }
+};
+</script>
+
+<style>
+</style>
