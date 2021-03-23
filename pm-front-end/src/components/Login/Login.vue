4de0d5a9ce7aa3af875b4f061ef1718c6583bad8
Srim <deamoon0801@naver.com>
2021-03-23 12:06:53 +0900

    [ADD] <Login>

--- pm-front-end/src/components/Login/Login.vue
+++ pm-front-end/src/components/Login/Login.vue
@@ -9,16 +9,34 @@
       <p class="description-text">로그인하여 프로젝트를 효율적으로 관리해보세요</p>
       <form>
         <p>아이디</p>
-        <input type="text" />
+        <input type="text" v-model="userId" @keyup.enter="onLogin" />
 
         <p>비밀번호</p>
-        <input type="password" />
+        <input type="password" v-model="password" @keyup.enter="onLogin" />
       </form>
-      <button>로그인</button>
+      <button @click="onLogin">로그인</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      userId: "",
+      password: ""
+    };
+  },
+  methods: {
+    ...mapActions(["LOGIN"]),
+    onLogin() {
+      console.log(this.userId, this.password);
+      this.userId === "" && this.password === ""
+        ? alert("아이디와 패스워드를 입력해주세요.")
+        : this.LOGIN({ userId: this.userId, password: this.password });
+    }
+  }
+};
 </script>
--- pm-front-end/src/lib/api/user.js
+++ pm-front-end/src/lib/api/user.js
@@ -1,12 +1,20 @@
 import axios from "axios";
 import { client } from "./client";
 
+export const login = (userId, password) => {
+  return client
+    .post("/user/auth", { userId, password })
+    .then(({ data }) => data)
+    .catch((err) => console.log(err));
+};
+
+export const setAuthInHeader = (accessToken) => {
+  axios.defaults.headers.common["Authorization"] = accessToken ? `Bearer ${accessToken}` : null;
+};
+
 export const signUp = (userId, password) => {
   return client
     .post("/user/register", { userId, password })
     .then(({ data }) => data)
-    .catch((err) => {
-      console.log(err);
-      console.log(userId, password);
-    });
+    .catch((err) => console.log(err));
 };
