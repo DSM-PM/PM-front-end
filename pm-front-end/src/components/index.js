eb97a73ead8f2a8755826824c1155e74ffb4ef6b
Srim <deamoon0801@naver.com>
2021-03-29 20:20:18 +0900

    [ADD] auth middleware

--- pm-front-end/src/components/index.js
+++ pm-front-end/src/components/index.js
@@ -4,3 +4,5 @@ export { default as Home } from "./Home/index.vue";
 export { default as Header } from "./Common/Header";
 export { default as Navbar } from "./Common/Navbar";
 export { default as Layout } from "./Layout";
+export { default as Issue } from "./Issue/index.vue";
+export { default as Kanban } from "./Kanban/index.vue";
--- pm-front-end/src/router/index.js
+++ pm-front-end/src/router/index.js
@@ -1,7 +1,16 @@
 import Vue from "vue";
 import Router from "vue-router";
-import { Login, SignUp, Layout, Home } from "@/components";
+import store from "../store";
+import { Login, SignUp, Layout, Home, Kanban, Issue } from "@/components";
 Vue.use(Router);
+
+const requireAuth = () => (from, to, next) => {
+  if (store.state.accessToken) next();
+  else {
+    alert("로그인하세요");
+    next("/");
+  }
+};
 
 export default new Router({
   mode: "history",
@@ -20,11 +29,22 @@ export default new Router({
       name: "Home",
       path: "/home",
       component: Layout,
+      beforeEnter: requireAuth(),
       children: [
         {
           name: "Main",
           path: "/home",
           component: Home,
+        },
+        {
+          name: "Kanban",
+          path: "/kanban",
+          component: Kanban,
+        },
+        {
+          name: "Issue",
+          path: "/issue",
+          component: Issue,
         },
       ],
     },
