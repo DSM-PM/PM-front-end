0ab13c29e9ec26c97d4dd86509e449618341427e
Srim <deamoon0801@naver.com>
2021-03-29 14:58:41 +0900

    [ADD] <src/assets> icon

--- pm-front-end/src/assets/img/index.js
+++ pm-front-end/src/assets/img/index.js
@@ -1,1 +1,5 @@
 export { default as sideViewImg } from "./side_view_img.svg";
+export { default as kanban } from "./kanban.svg";
+export { default as issue } from "./issue.svg";
+export { default as plus } from "./plus.svg";
+export { default as logout } from "./logout.svg";
