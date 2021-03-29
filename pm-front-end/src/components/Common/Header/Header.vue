e4914cc149a2beeb21f76bcdb56c5e6dc8d7535c
Srim <deamoon0801@naver.com>
2021-03-29 14:59:01 +0900

    [ADD] <Header> components

---
+++ pm-front-end/src/components/Common/Header/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <div>
+      <h1>
+        P
+        <p>roject</p>M
+        <p>anegment</p>
+      </h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+$yellow: #f5df4d;
+$gray: #939597;
+$font: "AppleSDGothicNeo";
+$titleFont: "Impact";
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-family: $titleFont;
+    font-size: 4rem;
+    display: flex;
+    align-items: baseline;
+    color: $yellow;
+    p {
+      color: $gray;
+      font-family: $titleFont;
+      font-size: 1.4rem;
+      margin: 0 0.2rem 0 0.4rem;
+    }
+  }
+}
+</style>
