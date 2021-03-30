1521f55be219784c1d27d1da1ad62d607567f6e7
Srim <deamoon0801@naver.com>
2021-03-30 16:07:12 +0900

    [ADD] <InputModal> UI

---
+++ pm-front-end/src/components/Common/Modal/InputModal.vue
@@ -0,0 +1,66 @@
+<template>
+  <transition name="modal" appear>
+    <div class="modal modal-overlay" @click.self="$emit('close')">
+      <div class="modal-window">
+        <div class="modal-content">
+          <slot />
+        </div>
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button @click="$emit('close')">Close</button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style lang="scss" scoped>
+.modal {
+  &.modal-overlay {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+  }
+}
+.modal-window {
+  background: #fff;
+  border-radius: 4px;
+  width: 20rem;
+  height: 12rem;
+  overflow: hidden;
+  padding: 1.2rem 2rem;
+}
+.modal-content {
+  width: 100%;
+}
+.modal-footer {
+  width: 100%;
+}
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.4s;
+}
+.modal-enter-active .modal-window,
+.modal-leave-active .modal-window {
+  transition: opacity 0.4s, transform 0.4s;
+}
+.modal-leave-active {
+  transition: opacity 0.6s ease 0.4s;
+}
+.modal-enter,
+.modal-leave-to {
+  opacity: 0;
+}
+.modal-enter .modal-window,
+.modal-leave-to .modal-window {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+</style>
--- pm-front-end/src/components/Home/Home.vue
+++ pm-front-end/src/components/Home/Home.vue
@@ -6,15 +6,76 @@
     <board-item />
     <board-item />
     <div class="board-item">
-      <a class="new-board-btn" href @click.prevent="onCreateBoard">Create new board...</a>
+      <button class="new-board-btn" href @click="openModal">Create new board...</button>
     </div>
+    <input-modal @close="closeModal" v-if="modal">
+      <h1>CREATE BOARD</h1>
+      <input v-model="boardName" placeholder="board name..." class="modal-input" />
+      <template slot="footer">
+        <button @click="onSendBoardName" class="modal-button">추가</button>
+      </template>
+    </input-modal>
   </div>
 </template>
 
 <script>
 import { BoardItem } from "./";
+import InputModal from "../Common/Modal/InputModal";
+import { WarningToast } from "../../lib/toast";
 export default {
   name: "Home",
-  components: { "board-item": BoardItem }
+  components: { "board-item": BoardItem, "input-modal": InputModal },
+  data() {
+    return {
+      modal: false,
+      boardName: ""
+    };
+  },
+  methods: {
+    openModal() {
+      this.modal = true;
+    },
+    closeModal() {
+      this.modal = false;
+    },
+    onSendBoardName() {
+      if (this.boardName.length > 0) {
+        console.log(this.boardName);
+        this.boardName = "";
+        this.closeModal();
+      } else {
+        WarningToast("보드 이름을 입력하세요.");
+      }
+    }
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+h1 {
+  color: #939597;
+  font-size: 1.4rem;
+}
+.modal-input {
+  margin: 1rem 0;
+  width: 100%;
+  padding: 1rem;
+}
+
+.modal-button {
+  background-color: #f5df4d;
+  box-shadow: 0px 3px 6px #00000016;
+  width: 100%;
+  color: white;
+  height: 2.3rem;
+  border-radius: 1rem;
+  outline: none;
+  cursor: pointer;
+  border: none;
+  opacity: 0.9;
+  &:hover {
+    opacity: 1;
+    transition: 0.3s;
+  }
+}
+</style>
