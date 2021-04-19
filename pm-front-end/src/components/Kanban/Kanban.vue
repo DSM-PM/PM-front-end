6db8b667811a9323e2d8399324d17f083111e9da
Srim <deamoon0801@naver.com>
2021-04-19 11:44:24 +0900

    [ADD] <Kanban> Detail UI

---
+++ pm-front-end/src/components/Kanban/Kanban.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="list">
+    <list v-for="(l, i) in lists" :key="i" :listInfo="l" />
+  </div>
+</template>
+
+<script>
+import List from "@/components/List/List.vue";
+export default {
+  components: { list: List },
+  data() {
+    return {
+      lists: [
+        {
+          title: "BACKLOG"
+        },
+        {
+          title: "TO DO"
+        },
+        {
+          title: "IN PROGRESS"
+        },
+        {
+          title: "DONE"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
