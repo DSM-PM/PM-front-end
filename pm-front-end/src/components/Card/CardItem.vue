c2201dd661fed14f7f9e5901e5e7063e1d5bd496
Srim <deamoon0801@naver.com>
2021-04-19 11:45:08 +0900

    [ADD] <Card> UI

---
+++ pm-front-end/src/components/Card/CardItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <p>로그인 UI 구현</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  background-color: white;
+  padding: 0.9rem;
+  width: 90%;
+  margin: 0.5rem auto 1rem auto;
+  height: 6rem;
+  border-radius: 0.6rem;
+  box-shadow: 1px 2px 10px #00000003;
+  cursor: pointer;
+
+  &:hover {
+    width: 92%;
+    height: 6.3rem;
+    box-shadow: 1px 2px 10px #00000015;
+    transition: 0.3s;
+  }
+}
+</style>
---
+++ pm-front-end/src/components/List/List.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="list-container">
+    <p class="list-container-title">{{listInfo.title}}</p>
+    <card />
+    <card />
+    <card />
+    <card />
+    <card />
+  </div>
+</template>
+
+<script>
+import CardItem from "../Card/CardItem";
+export default {
+  props: ["listInfo"],
+  components: { card: CardItem }
+};
+</script>
+
+<style lang="scss" >
+.list {
+  display: flex;
+  &-container {
+    overflow: scroll;
+    background-color: #93959710;
+    border-radius: 0.8rem;
+    margin-left: 2.5rem;
+    vertical-align: top;
+    width: 22%;
+    height: 49rem;
+    max-height: 100%;
+
+    &-title {
+      margin: 1rem 0 1rem 1rem;
+    }
+  }
+}
+</style>
